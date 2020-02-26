2c293f7e8d56a4e661c96811a32cf2496fd706a8
Krzych Pierzchala <krzysztof.pierzchala@likims.com>
2020-02-26 11:56:37 +0100

    add search form

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,28 @@
     <main class="w-8/12">
       <Post v-for="post in posts" :key="post.id" :post="post" />
     </main>
-    <aside class="px-6">
+    <aside class="px-6 w-4/12">
       <div>
         <h3 class="uppercase mb-2">most liked</h3>
         <ul class="text-sm">
-          <li v-for="post in getMostLiked(3)" :key="post.id" class="px-2 pb-2">
-            {{ post.title }} rating: {{ post.rating }}
-          </li>
+          <li
+            v-for="post in getMostLiked(3)"
+            :key="post.id"
+            class="px-2 pb-2"
+          >{{ post.title }} rating: {{ post.rating }}</li>
         </ul>
+      </div>
+      <div>
+        <form class="w-full my-4">
+          <input
+            type="text"
+            class="border border-solid border-gray-400 block bg-gray-300 w-full p-2 mb-4 tracking-wide"
+            placeholder="post title"
+          />
+          <button
+            class="bg-gray-500 hover:bg-gray-700 hover:text-gray-100 transition-all duration-200 ease-in py-2 px-6"
+          >Go</button>
+        </form>
       </div>
     </aside>
   </div>
